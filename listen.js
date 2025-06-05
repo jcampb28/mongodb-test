a4ee00f714a6a5f1027af776e9ee2e63196d7e9d
Jennifer Campbell <jennifercampbell28@hotmail.com>
2025-06-05 12:09:53 +0100

    required app into listen file

--- listen.js
+++ listen.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 console.log("MONGO_URL =", process.env.MONGO_URL);
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
+const app = require("./server/app")
 
 mongoose.connect(process.env.MONGO_URL)
 
